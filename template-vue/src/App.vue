3254d006215a147e577142bdc6889f19c0ac42a8
Shun Git <kwokshungit@gmail.com>
2020-08-01 15:32:44 +0800

    rename and change template-vue

--- template-vue/src/App.vue
+++ template-vue/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+<button @click="count++">count is {{ count }}</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const count = ref(0)
+
+    return {
+      count,
+    }
   }
 }
 </script>
